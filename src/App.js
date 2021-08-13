5a3bb5a16e3b1128c7d7fdf8de3a68be11743c41
mhji <battlesun99@gmail.com>
2021-08-13 17:38:01 +0900

    Feat: create sass component

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import SassComponent from "./SassComponent";
+
+function App() {
+  return (
+    <div className="App">
+      <SassComponent />
+    </div>
+  );
+}
+
+export default App;
