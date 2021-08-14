430c94daf7f27bc4916c7c9bf31931597c81f288
mhji <battlesun99@gmail.com>
2021-08-14 18:44:29 +0900

    Refactor: refactoring code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
-import './App.css';
-import SassComponent from './SassComponent';
+import "./style/App.scss";
+import SassComponent from "./Components/SassComponent";
+import CSSModule from "./Components/CSSModule";
+import StyledComponent from "./Components/StyledComponent";
+import Button from "./Components/Button";
 
 function App() {
   return (
     <div className="App">
       <SassComponent />
+      <CSSModule />
+      <StyledComponent />
+      <div className="Button-Container">
+        <div className="buttons">
+          <Button size="large" onClick={() => console.log("Clicked!")}>Button</Button>
+          <Button>Button</Button>
+          <Button size="small">Button</Button>
+        </div>
+        <div className="buttons">
+          <Button size="large" color="gray">Button</Button>
+          <Button color="gray">Button</Button>
+          <Button size="small" color="gray">Button</Button>    
+        </div>
+        <div className="buttons">
+          <Button size="large" color="pink">Button</Button>
+          <Button color="pink">Button</Button>
+          <Button size="small" color="pink">Button</Button>  
+        </div>
+        <div className="buttons">
+          <Button size="large" color="cyan" outline="true">Button</Button>
+          <Button color="cyan" outline="true">Button</Button>
+          <Button size="small" color="cyan" outline="true">Button</Button>  
+        </div>
+        <div className="buttons">
+          <Button size="large" color="orange" fullWidth="true">Button</Button>
+          <Button color="orange" fullWidth="true">Button</Button>
+          <Button size="small" color="orange" fullWidth="true">Button</Button>  
+        </div>
+      </div>
     </div>
   );
 }
